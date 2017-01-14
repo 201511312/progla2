8da9fc0fb41737a9ca27da715ec11bb448663484
Jamir Butch D. Agtina <jamagtina@yahoo.com>
2017-01-14 15:25:59 +0800

    Update file_append.cpp

--- file_append.cpp
+++ file_append.cpp
@@ -1,36 +1,21 @@
 #include<iostream>
-#include <stdio.h>
- main(){
+using namespace std;
+main()
+{
     FILE *myFile;
     myFile = fopen("sample.txt", "r");
-
     //read file into array
-    int i, sum=0;
-    
+    int i, sum = 0;
     int numberArray[5];
-
-    for (i = 0; i < 5; i++){
+    for(int i=0; i<5; i++){
         fscanf(myFile, "%d", &numberArray[i]);
-    }
-
-    for (i = 0; i < 5; i++){
-    	
-        printf( """%d\n", numberArray[i]);
-        
-    }
-	   
-	  for (i = 0; i < 5; i++){
-    	
         sum+=numberArray[i];
-        
     }
+	
     fclose(myFile);
     myFile = fopen("sample.txt", "a");
-     for (i = 0; i < 5; i++){
-    	 fprintf(myFile, "%d", numberArray[i]);
-		sum=4;
-        printf( """%d\n", sum+=numberArray[i]);
-        
+    for(int i=0; i<5; i++){
+		fprintf(myFile, "%d", numberArray[i]+4);
+		fprintf(myFile, "\n");
     }
-    
 }

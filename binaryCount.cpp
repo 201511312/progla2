688b6f2b956b7884b4e082fdf4ff435ddc2827b3
Jamir Butch D. Agtina <jamagtina@yahoo.com>
2017-01-21 17:16:58 +0800

    Update binaryCount.cpp

--- binaryCount.cpp
+++ binaryCount.cpp
@@ -2,15 +2,15 @@
 #include<cstring>
 using namespace std;
 int main(){
-	int count=0;
-	char binaryString[10];
-	strcpy(binaryString,"10100100101");
+   int count=0;
+   char binaryString[10];
+   strcpy(binaryString,"10100100101");
 	
-		for(int i=0;i<10;i++){
-				if(binaryString[i]=='1'){
-					count = count + 1;	
-				}
-		}
+	for(int i=0;i<10;i++){
+	if(binaryString[i]=='1'){
+	count = count + 1;	
+	 }
+	}
 		
-			cout<<count<<endl;		
+	cout<<count<<endl;		
 }

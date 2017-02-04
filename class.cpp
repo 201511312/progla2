a70cda607ec721725dc34c327e0dcc66193197ee
Jamir Butch D. Agtina <jamagtina@yahoo.com>
2017-02-04 18:42:07 +0800

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,34 +1,64 @@
-#include<stdio.h>
-#include<iostream>
+#include <iostream>
 #include<string>
 using namespace std;
-
-class person{
+class user{
 	public:
 		string name;
-		int id;
+		int id_num;
 };
-
-class student : public person{
-	private:
-		int books;
-		int subjects;
-		int tin;
-		int sss;
-		
+class student : public user{
 	public:
-		int stud1_num;
-		
-		int set_books(int books){
-			books = num;
-		};
-		int get_books(){
-			return books;
-		};
+		int subject;
+		int books;
 };
-
+class professor : public user{
+	public:
+		int tin;
+};
+class staff : public user{
+	public:
+		int sss;
+};
 int main(){
-	student stud1;
-	stud1.set_books(1);
-	cout<< stud1.get_books;
+		student stud;
+		professor prof;
+		staff Staff;
+			cout << "Student Name: ";
+			cin >> stud.name;
+			cout << "Input id num: ";
+			cin >> stud.id_num;
+			cout << "Number of subject: ";
+			cin >> stud.subject;
+			cout << "Number of books: ";
+			cin >> stud.books;
+			cout<< endl;
+			
+			cout << "Professor Name: ";
+			cin >> prof.name;
+			cout << "ID Number: ";
+			cin >> prof.id_num;
+			cout << "Tin: ";
+			cin >> prof.tin;
+			cout<<endl;
+		
+			cout << "Staff Name: ";
+			cin >> Staff.name;
+			cout << "ID Number: ";
+			cin >> Staff.id_num;
+			cout << "SSS: ";
+			cin >> Staff.sss;
+		
+			cout<<endl<<endl;
+			cout <<"Name of student: "<<stud.name<<endl;
+			cout << "ID Num: "<< stud.id_num<<endl;
+			cout << "Number of subject: "<<stud.subject<<endl;
+			cout << "Number of books: "<<stud.books<<endl<<endl;
+	
+			cout <<"Professor Name: "<<prof.name<<endl;
+			cout << "ID Num: "<<prof.id_num<<endl;
+			cout << "Tin: "<<prof.tin<<endl<<endl;
+			
+			cout <<"Staff Name: "<<Staff.name <<endl;
+			cout << "ID Num: "<<Staff.id_num<<endl;
+			cout << "SSS: "<<Staff.sss;
 }
